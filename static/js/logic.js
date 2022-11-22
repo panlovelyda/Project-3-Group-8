52fd79c4f50f213e5e3c13899e59e0592a4b13a0
Shaun <gotocooling@gmail.com>
2022-11-22 20:52:14 +1100

    add more period

--- static/js/logic.js
+++ static/js/logic.js
@@ -307,7 +307,7 @@ function trendLine(value){
 var element_suburb = document.getElementById("selSuburb");
 var element_subject = document.getElementById("selDataset");
 
-var selectList=['2020-2021','2011-2021','Growth PA','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020','2021']
+var selectList=['2011-2021','Growth PA','2021-2022','2020-2021','2019-2020','2018-2019','2017-2018','2016-2017','2015-2016','2014-2015','2013-2014','2012-2013','2011-2012','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020','2021']
 /* //let selectList = new Array();
 var selectList = ['test'];
 var selectList1 = new Array();
